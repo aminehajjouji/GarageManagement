f10555b7415de9aacbc7633e3a435553f2901152
mahajjouji <mahajjouji@sqli.com>
2025-08-29 09:03:09 +0100

    project init

---
+++ src/test/java/org/renault/garagemanagement/GarageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.renault.garagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
